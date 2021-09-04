8e08be4e71a4eb7675adc66bcc60f694e851bcc0
Jarosław <50953962+flame4ost@users.noreply.github.com>
2021-09-04 12:59:32 +0300

    initial commit

--- app/src/main/java/com/flame4ost/happyticket/MainActivity.java
+++ app/src/main/java/com/flame4ost/happyticket/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                             bulb.setImageDrawable(green);
                         }
                     }
+
                 } else {
                     bulb.setImageDrawable(red);
                 }
